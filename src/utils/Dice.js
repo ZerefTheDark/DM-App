35d15bc53981ea171085f259b1c63221dd22e49c
ZerefTheDark <senorlongslong45@gmail.com>
2025-09-08 01:14:14 -0400

    Add default parameters to rollDice and rollMultipleDice

--- src/utils/Dice.js
+++ src/utils/Dice.js
@@ -1,3 +1,13 @@
-export function rollDice(sides) {
+// src/utils/Dice.js
+
+export function rollDice(sides = 6) {
   return Math.floor(Math.random() * sides) + 1;
+}
+
+export function rollMultipleDice(numDice = 1, sides = 6) {
+  const results = [];
+  for (let i = 0; i < numDice; i++) {
+    results.push(rollDice(sides));
+  }
+  return results;
 }
